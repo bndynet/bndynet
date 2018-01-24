7d0d381e87ebba92c4891e414a4d7a92605a98c3
Bing Zhang <bingzha@cisco.com>
2018-01-24 18:26:26 +0800

    :v: new version

--- js/stats.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?93fe8707d8b666f04c1b1cce5232f665";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
